d93b392c27cb3fc2a3be55e24aa9eb3b99273f33
harriganeli-source <harrigan.eli@gmail.com>
2026-06-30 11:26:34 -0400

    Set phone mockup as og:image on all pages; remove logo 11 from rotation; add OG tags to inner pages

--- js/logo-rotate.js
+++ js/logo-rotate.js
@@ -2,8 +2,7 @@
     var logos = [
         'images/hogan logo 8.svg',
         'images/hogan logo 9.svg',
-        'images/hogan logo 10.svg',
-        'images/hogan logo 11.svg'
+        'images/hogan logo 10.svg'
     ];
     var picked = logos[Math.floor(Math.random() * logos.length)];
     document.querySelectorAll('.nav-logo-mark').forEach(function (mark) {
